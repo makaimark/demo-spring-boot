8ac34eee7f999340c0d916023a10a324e35ceeda
makaimark <makaimark@gmail.com>
2017-01-27 08:24:04 +0100

    removed not working elements from HelloBean class

--- src/main/java/spring/boot/example/HelloBean.java
+++ src/main/java/spring/boot/example/HelloBean.java
@@ -1,19 +1,11 @@
 package spring.boot.example;
 
-
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.stereotype.Component;
 
 @Component
-public class HelloBean implements EmbeddedServletContainerCustomizer {
+public class HelloBean {
 
-    @Override
-    public void customize(ConfigurableEmbeddedServletContainer container) {
-        container.setPort(61001);
-    }
-
-    public String sayHello() {
+    String sayHello() {
         return "Hello Márkifijúúú!";
     }
 }
