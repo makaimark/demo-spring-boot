ece982c33f3ec7039595f76261da23a98dfe79d6
makaimark <makaimark@gmail.com>
2017-01-27 20:21:54 +0100

    works without thymeleaf

--- src/main/java/spring/boot/example/Index.java
+++ src/main/java/spring/boot/example/Index.java
@@ -1,12 +1,17 @@
 package spring.boot.example;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@org.springframework.stereotype.Controller
+@Controller
 public class Index {
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index.html";
     }
+
 }
