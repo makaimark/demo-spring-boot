6d50a7aa1a591474444e5601188a482c15039121
makaimark <makaimark@gmail.com>
2017-01-28 13:36:33 +0100

    kk

--- src/main/java/spring/boot/example/Index.java
+++ src/main/java/spring/boot/example/Index.java
@@ -4,14 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class Index {
 
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "index.html";
+    public ModelAndView greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+        Map<String, String > params = new HashMap<>();
+        params.put("name", name);
+        return new ModelAndView("indexThymeleaf", params);
     }
 
 }
